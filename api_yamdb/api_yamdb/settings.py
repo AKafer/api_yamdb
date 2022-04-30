93272cef7dbd8d785ecf57d3ccf9a5942b00e966
Сергей Сторожук <akafer82@yandex.ru>
2022-04-30 21:18:58 +0300

    1

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -17,16 +17,15 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'reviews.apps.ReviewsConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'django_filters',
     'api',
-    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
